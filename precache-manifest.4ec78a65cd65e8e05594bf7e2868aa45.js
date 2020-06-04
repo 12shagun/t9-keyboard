4e28cb65268774e611c5661b63f9a979c2871d9e
12shagun <32757966+12shagun@users.noreply.github.com>
2020-06-05 01:31:07 +0530

    Updates

---
+++ precache-manifest.4ec78a65cd65e8e05594bf7e2868aa45.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64d92553b334a818d4f84ccfea19fdfa",
+    "url": "/t9-keyboard/index.html"
+  },
+  {
+    "revision": "e96f2f19c6026ba1ac65",
+    "url": "/t9-keyboard/static/css/main.018a71a1.chunk.css"
+  },
+  {
+    "revision": "719842d1481b4e82f497",
+    "url": "/t9-keyboard/static/js/2.8f8f7b63.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/t9-keyboard/static/js/2.8f8f7b63.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e96f2f19c6026ba1ac65",
+    "url": "/t9-keyboard/static/js/main.60361b4e.chunk.js"
+  },
+  {
+    "revision": "df269a734993ff9cb4df",
+    "url": "/t9-keyboard/static/js/runtime-main.e19b4730.js"
+  }
+]);
