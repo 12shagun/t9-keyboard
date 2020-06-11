607f636f6ec372a467d06f76316b7a9a3ffa21f9
12shagun <32757966+12shagun@users.noreply.github.com>
2020-06-11 06:51:03 +0530

    final changes done

--- src/App.js
+++ src/App.js
@@ -1,101 +1,16 @@
 import React from 'react';
-import Buttons from "./Buttons";
 import styled from "styled-components";
+import Buttons from "./Buttons";
 
-class App extends React.Component {
-  state={
-    text:"",
-    clicks:0,
-    startTime: new Date().getTime(),
-    
-  }
-  //function to check if its a long press then print the number or 
-  //first click or more than one clicks then print the respective character
-  showText= (characters,mouseDownTime,mouseUpTime)=>{
-    this.setState(prestate=>({clicks: prestate.clicks +1}));
-    const currentTime = new Date().getTime();
-    const string=[...this.state.text];
-    const lastChar= string[string.length-1];
-    const isButtonSame=characters.includes(lastChar);
-    let newText;
-    // for long press
-    if(mouseUpTime-mouseDownTime>=650)              
-    {
-      this.setState({clicks:1});
-       newText = [...this.state.text,characters[characters.length-1]];
-      this.setState({startTime: new Date().getTime()});
-    }
-    // for first click
-    else if(currentTime-this.state.startTime>700||!isButtonSame)
-    {
-      this.setState({clicks:1});
-       newText = [...this.state.text,characters[0]];
-      this.setState({startTime: new Date().getTime()});
-    }
-    // for further clicks
-    else
-    {
-
-       newText=[...this.state.text];
-      newText.pop();
-      newText.push(characters[this.state.clicks%characters.length]);
-      this.setState({startTime: new Date().getTime()});
-    }
-      this.setState({ text: newText });
-    
-  }
-  //function to clear last entered character
-  clear=()=>{
-    let string=[...this.state.text];
-    string.pop();
-    this.setState({text:string});
-  }
-  static defaultProps={
-    keypad:[
-      ["1","1",".,"],
-      ["abc2","2","abc"],
-      ["def3","3","def"],
-      ["ghi4","4","ghi"],
-      ["jkl5","5","jkl"],
-      ["mno6","6","mno"],
-      ["pqrs7","7","pqrs"],
-       ["tuv8","8","tuv"],
-       ["wxyz9","9","wxyz"],
-       ["*","*","+"],
-       [" 0","0","space"],
-       ["#","#","^"]
-    ]
-  }
-  
- render(){
-  //list of button names and characters stored in it
- 
-   let op=this.props.keypad.map((key)=><Buttons  keys={key} onclick={this.showText}/>);
-  
-  return (
-    <div style={{ marginLeft: "42%",marginTop: "14%"}}>
-      <div className="btn-toolbar mb-3" >
-        <Screen className="text" >{(this.state.text)?this.state.text:"Type"}|</Screen>
-        <StyledButtons onClick={this.clear}>Clear</StyledButtons>
-       </div>
-       <div style={{display:"flex",flexWrap:"wrap",width:"200px"}}>
-        {op}
-       </div>
-    </div>
-  );
- }
-}
-
-export default App;
-//styled-components for buttons
-const StyledButtons=styled.button`
+//Styled-components for buttons
+const StyledButton = styled.button`
   width: 55px;
   height: 50px;
   margin: 3px;
   border-radius: 12px;
   `;
-  //styled-components for text showing on screen
-  const Screen=styled.p`
+//Styled-components for text showing on screen
+const Screen = styled.p`
   background-color: rgb(230, 230, 241);
   margin-bottom: 10px; 
   width: 125px; 
@@ -105,3 +20,103 @@ const StyledButtons=styled.button`
   word-wrap: break-word;
   border-radius: 4px;
   `;
+//Styled-components for body
+const Keyboard = styled.div`
+  margin-left: 42%;
+  margin-top: 14%;
+`;
+//Styled-components for aligning buttons
+const ButtonsAlignment = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  width: 200px;
+`;
+
+
+class App extends React.Component {
+
+  //list of button names and characters stored in it
+  static defaultProps = {
+    keypad: [
+      ["1", "1", ".,"],
+      ["abc2", "2", "abc"],
+      ["def3", "3", "def"],
+      ["ghi4", "4", "ghi"],
+      ["jkl5", "5", "jkl"],
+      ["mno6", "6", "mno"],
+      ["pqrs7", "7", "pqrs"],
+      ["tuv8", "8", "tuv"],
+      ["wxyz9", "9", "wxyz"],
+      ["*", "*", "+"],
+      [" 0", "0", "space"],
+      ["#", "#", "^"]
+    ]
+  }
+
+  state = {
+    text: "",
+    clicks: 0,
+    startTime: new Date().getTime(),
+  }
+
+  /**
+   * TO print the number on long press and the respective character on first click or more clicks
+   * @param {string} characters (keys on the button cicked)
+   * @param {number} mouseDownTime (time when button is pressed)
+   * @param {number} mouseUpTime (time when button is released)
+   * @memberof App
+   */
+  showText = (characters, mouseDownTime, mouseUpTime) => {
+    this.setState(prestate => ({ clicks: prestate.clicks + 1 }));
+    const currentTime = new Date().getTime();
+    const { text: string } = this.state;
+    const lastChar = string[string.length - 1];
+    const isButtonSame = characters.includes(lastChar);
+    let { text: newText } = this.state;
+    // For long press
+    if (mouseUpTime - mouseDownTime >= 650) {
+      this.setState({ clicks: 1 });
+      newText = newText.concat(characters[characters.length - 1]);
+      this.setState({ startTime: new Date().getTime() });
+    }
+    // For first click
+    else if (currentTime - this.state.startTime > 700 || !isButtonSame) {
+      this.setState({ clicks: 1 });
+      newText = newText.concat(characters[0]);
+      this.setState({ startTime: new Date().getTime() });
+    }
+    // For further clicks
+    else {
+      newText = newText.slice(0, newText.length - 1);
+      newText = newText.concat(characters[this.state.clicks % characters.length]);
+      this.setState({ startTime: new Date().getTime() });
+    }
+    this.setState({ text: newText });
+
+  }
+  //Function to clear last entered character
+  clear = () => {
+    let { text: string } = this.state;
+    string = string.slice(0, string.length - 1);
+    this.setState({ text: string });
+  }
+
+  render() {
+    const { text } = this.state;
+    const { keypad } = this.props;
+
+    return (
+      <Keyboard>
+        <div className="btn-toolbar mb-3" >
+          <Screen className="text" >{(text) ? text : "Type"}|</Screen>
+          <StyledButton onClick={this.clear}>Clear</StyledButton>
+        </div>
+        <ButtonsAlignment>
+          {keypad.map((key, index) => <Buttons key={index} keys={key} onclick={this.showText} />)}
+        </ButtonsAlignment>
+      </Keyboard>
+    );
+  }
+}
+
+export default App;
--- src/Buttons.js
+++ src/Buttons.js
@@ -1,29 +1,38 @@
 import React from "react";
 import styled from "styled-components";
-class Buttons extends React.Component{
-    state={
-        mouseDownTime:0,
-        mouseUpTime:0
-    }
-    //calculate when is mouse pressed
-    mouseDown=()=>{
-        var current = new Date().getTime();
-        this.setState({mouseDownTime:current});
-      }
-    // calculate when is mouse released
-      mouseUp=()=>{
-        var current = new Date().getTime();
-        this.setState({mouseUpTime:current});
-      }
-    render(){
-        return(<StyledButtons onClick={()=>this.props.onclick(this.props.keys[0],this.state.mouseDownTime,this.state.mouseUpTime)} onMouseDown={this.mouseDown} onMouseUp={this.mouseUp}>{this.props.keys[1]}<br></br>{this.props.keys[2]}</StyledButtons>);
-    }
-}
-export default Buttons;
+
 //styled-components for buttons
-const StyledButtons=styled.button`
+const StyledButton = styled.button`
   width: 55px;
   height: 50px;
   margin: 3px;
   border-radius: 12px;
   `;
+
+class Buttons extends React.Component {
+  state = {
+    mouseDownTime: 0,
+    mouseUpTime: 0
+  }
+
+  /**
+   * Calculate time when is mouse pressed
+   * @memberof Buttons
+   */
+  mouseDown = () => {
+    const current = new Date().getTime();
+    this.setState({ mouseDownTime: current });
+  }
+  /**
+   * Calculate time when is mouse released
+   * @memberof Buttons
+   */
+  mouseUp = () => {
+    const current = new Date().getTime();
+    this.setState({ mouseUpTime: current });
+  }
+  render() {
+    return (<StyledButton onClick={() => this.props.onclick(this.props.keys[0], this.state.mouseDownTime, this.state.mouseUpTime)} onMouseDown={this.mouseDown} onMouseUp={this.mouseUp}>{this.props.keys[1]}<br></br>{this.props.keys[2]}</StyledButton>);
+  }
+}
+export default Buttons;
